df0121174da8afcac5171d147c6928bdb6e15415
Yasminesoussi <yasminesoussie71@gmail.com>
2026-07-22 11:05:22 +0100

    Mise à jour theme2

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { BRAND } from '../../core/brand/brand.config';
+import { ThemeService } from '../../core/theme/theme.service';
 
 @Component({
   selector: 'app-footer',
@@ -9,4 +10,10 @@ import { BRAND } from '../../core/brand/brand.config';
 export class Footer {
   readonly brand = inject(BRAND);
   readonly year = new Date().getFullYear();
+  readonly themeService = inject(ThemeService);
+
+  get supportEmail(): string {
+    const email = this.themeService.getTheme().email;
+    return email || this.brand.supportEmail;
+  }
 }
